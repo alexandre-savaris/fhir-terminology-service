af2286f45d0b87bc2a7b687728fbc64bab7f62a5
savaris <savaris@ubuntu-22-04-2.myguest.virtualbox.org>
2023-10-26 15:10:27 -0300

    Added mustache template.

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -4,6 +4,8 @@ import express from 'express';
 import * as utils from '../../lib/utils.js';
 // For reading/writing content from/to the filesystem.
 import * as fs from 'fs/promises';
+// For date and time formatting.
+import date from 'date-and-time';
 
 // Router for the administrative endpoints.
 const router = new express.Router()
@@ -14,17 +16,33 @@ router.post('/admin/codesystem', async (req, res) => {
     // TODO: review the HTTP response codes according to the specification.
     try {
 
+        // Read the Mustache templates.
+        const template = await utils.readTemplate('templates/OperationOutcome.mustache');
+        let filledTemplate = '';
+
         // For evaluation and adjustements.
         let codesystem = req.body;
 
         if(codesystem.resourceType !== "CodeSystem") {
+
             // Only CodeSystems are accepted.
-            return res.status(500).send("aaa");
+            // TODO: correct template.
+            filledTemplate = await utils.renderTemplate(template, {
+                div: `<div xmlns=\"http://www.w3.org/1999/xhtml\"><h1>Operation Outcome</h1><h2>ERROR</h2>FTS-001: Incorrect resource type found, expected &quot;CodeSystem&quot; but found &quot;CodeSystemm&quot;</div>`,
+                severity: "error",
+                code: "processing",
+                diagnostics: `FTS-001: Incorrect resource type found, expected "CodeSystem" but found "CodeSystemm"`
+            }, { });
+    
+            return res.status(400).send(filledTemplate);
         }
 
         // Insert or replace the content of the element "id" with the server-generated ID.
         codesystem.id = (++lastGeneratedId).toString();
-        console.log(codesystem.id);
+        // Insert metadata about the CodeSystem.
+        codesystem.meta = {};
+        codesystem.meta.versionId = '1';
+        codesystem.meta.lastUpdated = date.format(new Date(), 'YYYY-MM-DDTHH:mm:ss.SSSZZ');
 
         // Validate the CodeSystem structure using the FHIR® JSON schema.
         const { valid, errors } = await utils.validateTerminologyStructure(JSON.stringify(codesystem));
@@ -33,7 +51,6 @@ router.post('/admin/codesystem', async (req, res) => {
         }
 
         // Save the CodeSystem to disk.
-        console.log(process.env.TERMINOLOGIES_BASEPATH + '/codesystems/' + `${codesystem.id}.json`);
         await fs.writeFile(process.env.TERMINOLOGIES_BASEPATH + '/codesystems/' + `${codesystem.id}.json`,
             JSON.stringify(codesystem), { encoding: 'utf8' }, (err) => {
             if (err) {
@@ -42,7 +59,7 @@ router.post('/admin/codesystem', async (req, res) => {
         })
     
         // Success!
-        return res.status(201).send(codesystem.id);
+        return res.status(201).send(codesystem);
 
     } catch (e) {
         res.status(500).send(e);
