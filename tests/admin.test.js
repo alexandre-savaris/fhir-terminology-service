0699cabbacc5617113bf8bc0e90983b8d5b2c084
savaris <savaris@ubuntu-22-04-2.myguest.virtualbox.org>
2023-11-07 13:52:43 -0300

    Added new test.

--- tests/admin.test.js
+++ tests/admin.test.js
@@ -1,9 +1,22 @@
 // For reading/writing content from/to the filesystem.
 import * as fs from 'fs';
+// For path manipulation.
+import * as path from 'path';
 // For accessing the endpoints from app.
 import request from 'supertest';
 // For accessing the express instance.
 import app from '../src/app.js';
+
+// Count the loaded templates.
+test('Should load template files on startup', async () => {
+
+    // Retrieve a list of files with templates' specification.
+    const files = fs.readdirSync(`${process.env.TEMPLATES_BASEPATH}/`)
+        .filter(file => path.extname(file) === '.mustache');
+
+    // Assert that the available templates were loaded into memory.
+    expect(files.length).toBe(Object.keys(templates).length);
+});
 
 // POST a valid CodeSystem.
 test('Should receive a valid CodeSystem', async () => {
@@ -16,4 +29,4 @@ test('Should receive a valid CodeSystem', async () => {
         .post('/admin/codesystem')
         .send(JSON.parse(fileContent.toString()))
         .expect(201);
-});
+}, 10000);
